fa5d3590a9eea869bb5bea2440dda0f5e92da357
Bluedog2121 <33208732+Bluedog2121@users.noreply.github.com>
2017-11-01 20:02:06 -0400

    Set the size and location of labels
    All labels that are currently declared will resize according to the size of the frame.

--- src/remoteAccesDatabaseServer/ServerSide.java
+++ src/remoteAccesDatabaseServer/ServerSide.java
@@ -1,7 +1,10 @@
 package remoteAccesDatabaseServer;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
@@ -16,8 +19,6 @@ public class ServerSide extends JFrame{
 	ServerSide(){
 		//Set JFrame to be Visible
 		setVisible(true);
-		//
-		setSize(1300, 1300);
 		//Maximize JFrame to be full screen
 		setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		//
@@ -29,9 +30,6 @@ public class ServerSide extends JFrame{
 		//Log title
 		JLabel log = new JLabel("Server Access Log - click here for full log" + "\n" + "Latest Entries on Log" );
 		log.setHorizontalAlignment(SwingConstants.CENTER);
-		log.setLocation(0, 0);
-		//Label Set Size 	(h,w)
-		log.setSize(648, 100);
 		//Set Border
 		Border black = BorderFactory.createLineBorder(Color.BLACK, 1);
 		log.setBorder(black);
@@ -42,20 +40,12 @@ public class ServerSide extends JFrame{
 		//Log title
 		JLabel action = new JLabel("Action" );
 		action.setHorizontalAlignment(SwingConstants.CENTER);
-		//Label Set Bounds	 (x,y,h,w)
-		action.setLocation(689, 0);
-		//
-		action.setSize(648, 100);
 		//Set Border
 		action.setBorder(black);
 		//Add Label to Container C
 		c.add(action);
 		
 		JPanel left = new JPanel();
-		//JPanel left Set Size
-		left.setSize(648,644);
-		//JPanel Left Set Location
-		left.setLocation(0,99);
 		//JPanel Left set Border to Border Black
 		left.setBorder(black);
 		//add Left to container c
@@ -63,16 +53,41 @@ public class ServerSide extends JFrame{
 		
 		
 		JPanel right = new JPanel();
-		//JPanel right Set Size
-		right.setSize(getWidth()/2, (int)(getHeight()*0.75));
-		//JPanel right Set Location
-		right.setLocation((getWidth()/2) + 1, (int)(getHeight()*0.25) + 1);
 		//JPanel right set Border to Border Black
 		right.setBorder(black);
 		//add right to container c
 		c.add(right);
 		
+		getContentPane().addComponentListener(new ComponentAdapter() {
+			public void componentResized(ComponentEvent e) {
+				Component c = (Component)e.getSource();
+					//Log
+				//Label set location
+				log.setLocation(0, 0);
+				//Label Set Size 	(w, h)
+				log.setSize(getWidth()/2, (int)(getHeight()*0.10));
+					//Action
+				//Label Set Bounds	 (x,y,w,h)
+				action.setLocation((getWidth()/2), 0);
+				//Action set size
+				action.setSize(getWidth()/2, (int)(getHeight()*0.10));
+					//Left
+				//JPanel left Set Size
+				left.setSize(getWidth()/2, (int)(getHeight()*0.90));
+				//JPanel Left Set Location
+				left.setLocation(0, (int)(getHeight()*0.10));
+					//Right
+				//JPanel right Set Size
+				right.setSize(getWidth()/2, (int)(getHeight()*0.90));
+				//JPanel right Set Location
+				right.setLocation((getWidth()/2), (int)(getHeight()*0.10));
+			}
+		});
+		
 		
 		add(c);
+		pack();
+		//frame set size
+		setSize(1300, 1300);
 	}
 }
