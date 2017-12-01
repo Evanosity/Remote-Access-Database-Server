efbbe1e1aedc216fd3c5628d9965de6d05f49aee
Evanosity <iamevanosity@gmail.com>
2017-12-01 13:24:47 -0500

    more framework work
    basically nothing tbh

--- src/remoteAccesDatabaseServer/Networking.java
+++ src/remoteAccesDatabaseServer/Networking.java
@@ -1,7 +1,26 @@
 package remoteAccesDatabaseServer;
 
+/**
+ * public class Networking - this is the framework for networking, and it just has the very basic receive/send methods. These will be used
+ * by other classes to effectively send and receive data.
+ * @author Evanosity
+ * @date December 1 2017
+ */
 public class Networking {
 	public Networking(){
-		//this will be fun.
+		//setup
+	}
+	
+	public String receiveMessage(){
+		//this will receive a message, or wait to receive a message.
+		return null;
+	}
+	
+	public void sendMessage(String toSend){
+		
+	}
+	
+	public void shutdown(){
+		
 	}
 }
