646d7fecf9bae4b588966a2a05338b690d748ac8
camham <31860003+camham@users.noreply.github.com>
2017-12-15 12:30:21 -0500

    Does not need to run reisterUser

--- src/remoteAccesDatabaseServer/Main.java
+++ src/remoteAccesDatabaseServer/Main.java
@@ -3,7 +3,7 @@ package remoteAccesDatabaseServer;
 public class Main {	
 	public static void main(String[] args){
 		ServerSide ss = new ServerSide();
-		ss.registerUser();
+		//ss.registerUser();
 		
 		DatabaseAccess e=new DatabaseAccess("","","");
 		e.closeDatabase();
