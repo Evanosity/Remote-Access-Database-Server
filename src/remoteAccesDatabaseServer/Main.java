410c484f5b37e18d000f1ab3c4fd80df3a8a52ad
camham <31860003+camham@users.noreply.github.com>
2017-12-27 14:16:40 -0500

    Testing
    Tested stuff

--- src/remoteAccesDatabaseServer/Main.java
+++ src/remoteAccesDatabaseServer/Main.java
@@ -6,22 +6,24 @@ import javax.swing.JFrame;
 
 public class Main {	
 	
-	private final static int port = 6066;
+	private final static int portNet = 6066;	//Networking port
 	private static NetworkServer ns;
 	private static ServerSide ss;
 	private static DatabaseAccess db;
+	private final static String location = "localhost";
+	private final static String username = "SWDB_API_USER";
+	private final static String password = "dean22";
+	private final static int portData = 1521;	//Database Access Port
+	private static String table = "SWDB_System";
 	
 	public static void main(String[] args){
 		ss = new ServerSide();
 		
 		try {
-			 ns = new NetworkServer(port);
+			 ns = new NetworkServer(portNet);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		db = new DatabaseAccess("SWDB_System");
-				
-		//DatabaseAccess e=new DatabaseAccess("","","");
-		//e.closeDatabase();
+		db = new DatabaseAccess(location, username, password, portData, table);
 	}
 }
