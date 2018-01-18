cf51374f460d29b297851ee7b15637cab69068e6
Evanosity <iamevanosity@gmail.com>
2018-01-17 20:56:33 -0500

    Cleaned up, made it work
    this should work. I guess we'll find out when we test.

--- src/remoteAccesDatabaseServer/Main.java
+++ src/remoteAccesDatabaseServer/Main.java
@@ -18,8 +18,7 @@ public class Main {
 	
 	
 	public static void main(String[] args){
-		
-db = new DatabaseAccess("localhost", "SWDB_API_USER", "dean22", 1521, "SWDB_SYSTEM");
+		db = new DatabaseAccess("localhost", "SWDB_API_USER", "dean22", 1521, "SWDB_SYSTEM");
 		
 		String info[][] = null;
 		if(db.select() != null) {
@@ -32,111 +31,19 @@ db = new DatabaseAccess("localhost", "SWDB_API_USER", "dean22", 1521, "SWDB_SYST
 			System.out.println();
 		}
 		
-		
-		/*
-		String[][][] toSend=new String[5][5][5];
-		
-		for(int i=0; i!=5; i++){
-			for(int k=0; k!=5;k++){
-				for(int l=0; l!=5;l++){
-					toSend[i][k][l]=i+" "+k+" "+l+" ";
-				}
-			}
-		}
-		*/
-		
-		
 		try {
 			ns=new NetworkServer(portNet);
+			ns.sendDoubleArray(info);
+			String[][]toSend=ns.receiveDoubleArray();
+			for(int i=0;i!=3;i++){
+				db.update(toSend[i]);
+			}
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
 		
-		/*
-		
-		try {
-			ns.sendSuperArray();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			ns.shutdown();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		*/
-		
 		ss = new ServerSide();
-		
-		
-		
-		try {
-			 ns = new NetworkServer(portNet);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		// Doesn't contain correct variables 
-		db = new DatabaseAccess("localhost", "SWDB_API_USER", "dean22", 1521, "SWDB_SYSTEM");
-				
-		//DatabaseAccess e=new DatabaseAccess("","","");
-		//e.closeDatabase();
-	   //	db = new DatabaseAccess(location, username, password, portData, table);
-		
-		
-		/*
-		try {
-			doTheThing(db.select());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
-		
-
-	}
-	
-	/**
-	 * public void doTheThing - this is the method that runs the entire database. It waits for a message, then sends back the appropriate info. It then waits to receive the returned array, saves it properly and then loops back to the start.
-	 * @throws IOException
-	 */
-	public static void doTheThing(String send[][][])throws IOException{
-		String received;
-		String[][] parameters;
-		String[][][]toSend = send;
-		
-		//the client sends a message containing first what the info is, and then any additional shit. For example, for sending info to store, it will have that it's sending info to store, and then the number of incoming packages. I don't think packet loss will be a thing, so we should be good.
-		
-		while(true){
-			received=ns.receiveMessage();
-
-			if(true){
-				parameters=decodeParameters(received);
-				//send those parameters to the database
-				ns.sendSuperArray(toSend);
-			}
-			if(true){
-				//TODO - actually add this lul
-				//ns.receiveSuperArray(x, y, z);
-			}
-		}
-	}
-	
-	/**
-	 * public void decodeParameters - this method turns a single string into a much easier to read 2d string array containing all the parameters to search for in the database.
-	 * The first value is the actual parameter. The second is the value held by the parameter. The third is equals/greater than/smaller than for that parameter. The 4th value is a logical comparison between 2/3 and 5/6 - or, and, etc. The 5th and 6th are a copy of 2 and 3, so that you can have one parameter have multiple values. IE; is greater than 5 AND lower than 10. Is greater than 10 OR lower than 5
-	 * @param toDecode
-	 * @return
-	 */
-	public static String[][] decodeParameters(String toDecode){
-		//TODO - make this method.
-		String[][]toSend=null;
-		return toSend;
 	}
 	
 	public void Shutdown() {
@@ -145,7 +52,7 @@ db = new DatabaseAccess("localhost", "SWDB_API_USER", "dean22", 1521, "SWDB_SYST
 			db.closeDatabase();
 			ns.shutdown();
 		}catch(Exception e) {
-			
+			//cry I guess?
 		}
 	}
 }
